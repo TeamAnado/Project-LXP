03f64366f2eaeaf0db353cf85f71ac9a6aab7a60
dd-jiyun <dlwldbs8015@gmail.com>
2025-10-15 15:16:48 +0900

    refactor: Base Entity final 키워드 추가

--- src/main/java/com/lxp/base/BaseDateModifiedEntity.java
+++ src/main/java/com/lxp/base/BaseDateModifiedEntity.java
@@ -5,6 +5,11 @@ import java.time.LocalDateTime;
 public abstract class BaseDateModifiedEntity extends BaseDateCreatedEntity {
     private LocalDateTime dateModified;
 
+    protected BaseDateModifiedEntity(Long id) {
+        super(id);
+        this.dateModified = this.getDateCreated();
+    }
+    
     public void recordDateModified() {
         this.dateModified = LocalDateTime.now();
     }
