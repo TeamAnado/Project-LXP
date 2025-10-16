7df043cf7c49964cc672e2860adfe6a930db6479
dd-jiyun <dlwldbs8015@gmail.com>
2025-10-16 10:22:04 +0900

    fix: db connection 정적 생성으로 변경

--- src/main/java/com/lxp/Application.java
+++ src/main/java/com/lxp/Application.java
@@ -2,7 +2,6 @@ package com.lxp;
 
 import com.lxp.config.DBConfig;
 import com.lxp.exception.LXPExceptionHandler;
-
 import java.sql.Connection;
 import java.util.Scanner;
 
@@ -10,8 +9,7 @@ public class Application {
 
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in);
-             DBConfig dbConfig = new DBConfig();
-             Connection connection = dbConfig.getConnection()) {
+             Connection connection = DBConfig.getInstance().getConnection()) {
 
             //todo 기능 정의
 
