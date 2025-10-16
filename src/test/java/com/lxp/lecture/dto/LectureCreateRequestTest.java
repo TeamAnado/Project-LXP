a6e4dc31d8f8b5c654a6268645af225e11ab533e
dd-jiyun <dlwldbs8015@gmail.com>
2025-10-16 20:53:28 +0900

    test: 유효한 값 입력 생성

--- src/test/java/com/lxp/lecture/dto/LectureCreateRequestTest.java
+++ src/test/java/com/lxp/lecture/dto/LectureCreateRequestTest.java
@@ -1,10 +1,13 @@
 package com.lxp.lecture.dto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.lxp.exception.LXPException;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -20,10 +23,27 @@ class LectureCreateRequestTest {
         );
     }
 
+    @Test
+    @DisplayName("유효한 인자로 강의 생성을 시도하면 성공한다.")
+    void shouldCreate_whenValidArguments() {
+        // given
+        String title = "Valid Title";
+        String description = "Valid Description";
+
+        // when
+        LectureCreateRequest request = new LectureCreateRequest(title, description);
+
+        // then
+        assertAll(
+                () -> assertThat(request.title()).isEqualTo(title),
+                () -> assertThat(request.description()).isEqualTo(description)
+        );
+    }
+
     @ParameterizedTest
     @MethodSource("invalidLectureArguments")
     @DisplayName("유효하지 않은 인자로 강의 생성을 시도하면 예외가 발생한다.")
-    void createWithInvalidArguments(String title, String description, String expectedMessage) {
+    void shouldThrowException_whenInvalidArguments(String title, String description, String expectedMessage) {
         // when & then
         assertThatThrownBy(() -> new LectureCreateRequest(title, description))
                 .isInstanceOf(LXPException.class)
